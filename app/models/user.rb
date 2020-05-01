2b5915dbb8d554c34aaf5f1fc6eff9d9f88f812e
uchimt <dosanco.ucchi@gmail.com>
2020-05-02 00:24:42 +0900

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, uniqueness: true
+  
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
