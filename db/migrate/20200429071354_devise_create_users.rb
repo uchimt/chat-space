28ab729d6075d7c6867957d316de81c46c0131b8
uchimt <dosanco.ucchi@gmail.com>
2020-05-17 18:27:04 +0900

    de

--- db/migrate/20200429071354_devise_create_users.rb
+++ db/migrate/20200429071354_devise_create_users.rb
@@ -3,7 +3,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      # t.string :name,               null: false, index: true
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
