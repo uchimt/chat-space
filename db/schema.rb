d414e4f3ef5e8441a0f08b022e3b477edbbc7203
uchimt <dosanco.ucchi@gmail.com>
2020-05-17 15:19:42 +0900

    de

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,6 @@ ActiveRecord::Schema.define(version: 20200502071258) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "group_users", "groups"
