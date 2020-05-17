f84cbc25adef4d7a171e23b0bd3b3c364a41bf75
uchimt <dosanco.ucchi@gmail.com>
2020-05-17 19:03:28 +0900

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 # config valid for current version and patch releases of Capistrano
 lock "3.14.0"
-
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -11,11 +10,11 @@ set :repo_url,  'git@github.com:uchimt/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' 
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/deploy.pem'] 
+                  keys: ['~/.ssh/deploytes.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
